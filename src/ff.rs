7df0928fcd5bdc346f724b87e8a3ae76412d4951
Anjan Roy <anjanroy@yandex.com>
2021-12-14 03:47:42 +0000

    to canonical representation of prime field elements

--- src/ff.rs
+++ src/ff.rs
@@ -1,5 +1,10 @@
 use std::simd::Simd;
 
+const ULONG_MAX: u64 = 0xffffffffffffffff;
+const MOD: u64 = 0xffffffff00000001;
+const ZEROS: Simd<u64, 16> = Simd::from_array([0u64; 16]);
+const ONES: Simd<u64, 16> = Simd::from_array([1u64; 16]);
+
 #[inline]
 fn mul_hi(a: Simd<u64, 16>, b: Simd<u64, 16>) -> Simd<u64, 16> {
   let a_lo = a & 0x00000000ffffffff;
@@ -20,10 +25,6 @@ fn mul_hi(a: Simd<u64, 16>, b: Simd<u64, 16>) -> Simd<u64, 16> {
 
 #[inline]
 fn vec_mul_ff_p64(a: Simd<u64, 16>, b: Simd<u64, 16>) -> Simd<u64, 16> {
-  const zeros: Simd<u64, 16> = Simd::from_array([0u64; 16]);
-  const ones: Simd<u64, 16> = Simd::from_array([1u64; 16]);
-  const ULONG_MAX: u64 = 0xffffffffffffffff;
-
   let ab = a * b;
   let cd = mul_hi(a, b);
   let c = cd & 0x00000000ffffffff;
@@ -31,14 +32,23 @@ fn vec_mul_ff_p64(a: Simd<u64, 16>, b: Simd<u64, 16>) -> Simd<u64, 16> {
 
   let tmp0 = ab - d;
   let under0 = ab.lanes_lt(d);
-  let tmp1 = under0.select(ones, zeros) * 0xffffffff;
+  let tmp1 = under0.select(ONES, ZEROS) * 0xffffffff;
   let tmp2 = tmp0 - tmp1;
 
   let tmp3 = (c << 32) - c;
 
   let tmp4 = tmp2 + tmp3;
-  let over0 = tmp2.lanes_lt(ULONG_MAX - tmp3);
-  let tmp5 = over0.select(ones, zeros) * 0xffffffff;
+  let over0 = tmp2.lanes_gt(ULONG_MAX - tmp3);
+  let tmp5 = over0.select(ONES, ZEROS) * 0xffffffff;
 
   tmp4 + tmp5
+}
+
+#[inline]
+fn to_canonical(a: Simd<u64, 16>) -> Simd<u64, 16> {
+  // Following two lines are equivalent to `a % MOD`
+  //
+  // Just to avoid expensive modulo reduction, I use following
+  let over = a.lanes_ge(Simd::from_array([MOD; 16]));
+  a - over.select(ONES, ZEROS) * MOD
 }
