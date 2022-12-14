b5078ebfde2983ad1ed74b4951503324e9976514
Anjan Roy <hello@itzmeanjan.in>
2022-12-14 13:21:30 +0400

    modulo addition over prime field
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/ff.hpp
+++ include/ff.hpp
@@ -20,6 +20,22 @@ struct ff_t
 
   // Generate field element having canonical value 1
   static inline constexpr ff_t one() { return ff_t{ 1ul }; }
+
+  // Addition over prime field, both input operands and result to stay in
+  // canonical form
+  inline constexpr ff_t operator+(const ff_t& rhs) const
+  {
+    const uint64_t t0 = this->v + rhs.v;
+
+    const bool flg0 = this->v > UINT64_MAX - rhs.v;
+    const uint64_t t1 = static_cast<uint64_t>(-static_cast<uint32_t>(flg0));
+    const uint64_t t2 = t0 + t1;
+
+    const bool flg1 = t2 >= Q;
+    const uint64_t t3 = t2 - flg1 * Q;
+
+    return ff_t{ t3 };
+  }
 };
 
 }
