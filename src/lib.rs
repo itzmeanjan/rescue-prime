3f38aa3e6f0d64a63320b75179e4d7c99098aad9
Anjan Roy <hello@itzmeanjan.in>
2022-12-14 12:17:01 +0400

    clear canvas
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![feature(portable_simd)]
-
-mod ff;
-
-mod rescue_constants;
-pub use rescue_constants::{prepare_ark1, prepare_ark2, prepare_mds};
-
-mod rescue_prime;
-pub use rescue_prime::{hash_elements, merge};
