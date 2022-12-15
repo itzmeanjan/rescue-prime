32d372bcc8b340085de176cc2b270b24936c0304
Anjan Roy <hello@itzmeanjan.in>
2022-12-15 11:24:59 +0400

    check correctness of rescue permutation implementation using test vector
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/test/test_permutation.hpp
+++ include/test/test_permutation.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "ff.hpp"
 #include "permutation.hpp"
 #include <cassert>
 
@@ -16,6 +15,28 @@ test_alphas()
   const auto v_inv_alpha = v_alpha ^ rescue::INV_ALPHA;
 
   assert(v == v_inv_alpha);
+}
+
+// Check Rescue permutation using known answer test
+//
+// Test vector taken from
+// https://github.com/novifinancial/winterfell/blob/437dc08/crypto/src/hash/rescue/rp64_256/tests.rs#L47-L83
+void
+test_permutation()
+{
+  constexpr ff::ff_t expected[rescue::STATE_WIDTH]{
+    10809974140050983728ul, 6938491977181280539ul,  8834525837561071698ul,
+    6854417192438540779ul,  4476630872663101667ul,  6292749486700362097ul,
+    18386622366690620454ul, 10614098972800193173ul, 7543273285584849722ul,
+    9490898458612615694ul,  9030271581669113292ul,  10101107035874348250ul,
+  };
+  ff::ff_t state[rescue::STATE_WIDTH]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+  rescue::permute(state);
+
+  for (size_t i = 0; i < rescue::STATE_WIDTH; i++) {
+    assert(state[i] == expected[i]);
+  }
 }
 
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,5 @@
 #include "test/test_ff.hpp"
+#include "test/test_permutation.hpp"
 #include <iostream>
 
 int
@@ -6,6 +7,10 @@ main()
 {
   test_rphash::test_field_ops<4096>();
   std::cout << "[test] Rescue Prime field arithmetic\n";
+
+  test_rphash::test_alphas();
+  test_rphash::test_permutation();
+  std::cout << "[test] Rescue Permutation\n";
 
   return EXIT_SUCCESS;
 }
