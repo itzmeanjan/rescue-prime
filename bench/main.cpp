f2960a16248e98f301db64afb250b875ecf85768
Anjan Roy <hello@itzmeanjan.in>
2022-12-16 10:49:09 +0400

    register for benchmarking Rescue Prime element hasher function with variable number of input elements
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -4,4 +4,12 @@
 // Register for benchmarking Rescue permutation
 BENCHMARK(bench_rphash::permutation)->UseManualTime();
 
+// Register for benchmarking Rescue Prime element hasher
+BENCHMARK(bench_rphash::hash)->Arg(4)->UseManualTime();
+BENCHMARK(bench_rphash::hash)->Arg(8)->UseManualTime();
+BENCHMARK(bench_rphash::hash)->Arg(16)->UseManualTime();
+BENCHMARK(bench_rphash::hash)->Arg(32)->UseManualTime();
+BENCHMARK(bench_rphash::hash)->Arg(64)->UseManualTime();
+BENCHMARK(bench_rphash::hash)->Arg(128)->UseManualTime();
+
 BENCHMARK_MAIN();
