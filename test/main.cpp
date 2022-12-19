2f733ecde158fdf88891703557f5dc72303670ce
Anjan Roy <hello@itzmeanjan.in>
2022-12-19 18:36:00 +0400

    test AVX2 implementation only when explicitly asked for
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -8,7 +8,7 @@ main()
   test_rphash::test_field_ops();
   std::cout << "[test] Rescue Prime field arithmetic\n";
 
-#if defined __AVX2__
+#if defined __AVX2__ && USE_AVX2 != 0
 
   test_rphash::test_avx_mod_add();
   test_rphash::test_u64_full_mul();
