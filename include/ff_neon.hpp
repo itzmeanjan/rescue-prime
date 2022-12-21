e68e38c9c55863b08bc3db10e175d79c465691d4
Anjan Roy <hello@itzmeanjan.in>
2022-12-21 15:52:44 +0400

    modulo addition over Z_q, using NEON intrinsics
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/ff_neon.hpp
+++ include/ff_neon.hpp
@@ -42,6 +42,29 @@ struct ff_neon_t
     v = vld1q_u64(reinterpret_cast<const uint64_t*>(arr));
   }
 
+  // Given two 128 -bit registers, each holding two prime field Z_q elements,
+  // this routine performs element wise addition over Z_q and returns result in
+  // canonical form i.e. each 64 -bit result limb must ∈ Z_q.
+  //
+  // This routine does what
+  // https://github.com/itzmeanjan/rescue-prime/blob/22b7aa5/include/ff.hpp#L73-L84
+  // does, only difference is that instead of working on single element at a
+  // time, it works on two of them.
+  inline ff_neon_t operator+(const ff_neon_t& rhs) const
+  {
+    const auto u64x2 = vdupq_n_u64(UINT64_MAX);
+
+    const auto t0 = vaddq_u64(this->v, rhs.v);
+
+    const auto t1 = vsubq_u64(u64x2, rhs.v);
+    const auto t2 = vcgtq_u64(this->v, t1);
+    const auto t3 = vshrq_n_u64(t2, 32);
+    const auto t4 = vaddq_u64(t0, t3);
+
+    const auto t5 = reduce(t4);
+    return ff_neon_t{ t5 };
+  }
+
   // Stores two prime field Z_q elements ( kept in a 128 -bit register )
   inline void store(ff::ff_t* const arr) const
   {
