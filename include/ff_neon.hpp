169b67a80fe25aa6d964f6e465b1048a160b3bd4
Anjan Roy <hello@itzmeanjan.in>
2022-12-21 15:50:02 +0400

    reduce 64 -bit unsigned integers using NEON intrinsics
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/ff_neon.hpp
+++ include/ff_neon.hpp
@@ -7,6 +7,25 @@
 // Prime Field ( i.e. Z_q ) Arithmetic | q = 2^64 - 2^32 + 1
 namespace ff {
 
+// Given a 128 -bit register, holding two 64 -bit unsigned integers, this
+// routine converts each of those two limbs into their canonical representation
+// in prime field Z_q, by reducing using prime modulus Q = 2^64 - 2^32 + 1
+//
+// This routine does exactly what
+// https://github.com/itzmeanjan/rescue-prime/blob/22b7aa5/include/ff.hpp#L62-L64
+// does, but it reduces two field elements at a time.
+static inline uint64x2_t
+reduce(const uint64x2_t a)
+{
+  const auto q = vdupq_n_u64(ff::Q);
+
+  const auto t0 = vcgeq_u64(a, q);
+  const auto t1 = vandq_u64(t0, q);
+  const auto t2 = vsubq_u64(a, t1);
+
+  return t2;
+}
+
 // Two elements of prime field Z_q | q = 2^64 - 2^32 + 1, stored in a 128 -bit
 // Neon register, defining modular {addition, multiplication} over it.
 struct ff_neon_t
